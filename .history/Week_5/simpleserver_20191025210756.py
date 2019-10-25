347c0408d71eb7c38e7a7dcf91cec6960f624a64
jruther <justin.t.rutherford@gmail.com>
2019-10-25 21:22:30 +0100

    new material

---
+++ .history/Week_5/simpleserver_20191025210756.py
@@ -0,0 +1,18 @@
+#follow along from Andrew's lab.  JR 25.10.19
+
+#!flask/bin/python
+
+from flask import Flask
+app = Flask(__name__, static_url_path='', static_folder= '../')
+
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+    
+if _name_=='_main_' :
+    app.run(debug = True)
+
+
+
+
